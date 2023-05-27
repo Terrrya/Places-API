d88d6985e4b8839014f4b3e3bbcb0994de9881c4
Denys Tereshchuk <terrya@ukr.net>
2023-05-28 01:36:03 +0400

    Changed geom to PointField

--- places/models.py
+++ places/models.py
@@ -4,4 +4,7 @@ from django.contrib.gis.db import models
 class Place(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    geom = models.PolygonField()
+    geom = models.PointField()
+
+    def __str__(self) -> str:
+        return self.name
